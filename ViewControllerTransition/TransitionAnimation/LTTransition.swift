ac9abd1787e2590d4d232a55b1853c1367e06d95
yichahucha <xlitao@icloud.com>
2018-04-20 16:07:02 +0800

    注释

--- ViewControllerTransition/TransitionAnimation/LTTransition.swift
+++ ViewControllerTransition/TransitionAnimation/LTTransition.swift
@@ -11,6 +11,7 @@ import UIKit
 
 extension UIViewController {
     
+    //key
     private struct AssociaKey{
         static var kInteractiveTransitionKey = "kInteractiveTransitionKey"
         static var kAnimationManagerKey = "kAnimationManagerKey"
